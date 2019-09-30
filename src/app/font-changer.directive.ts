51d42ea41ac7e64ddb6136715fb17d3512f77f8c
bchizi <leonbichanga@gmail.com>
2019-09-30 09:36:58 +0300

    added a service repo-http

--- src/app/font-changer.directive.ts
+++ src/app/font-changer.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive ,HostListener,ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appFontChanger]'
 })
 export class FontChangerDirective {
 
-  constructor() { }
-
+  constructor(private elem:ElementRef) { 
+    this.elem.nativeElement.style.fontFamily = 'Cookie'
+    this.elem.nativeElement.style.fontSize = '20px'
+  // console.log(elem)
+}
+@HostListener("mousehover") onmousehover(){
+  this.fontChange('Caveat')
+}
+@HostListener("mouseout") onmouseout(){
+  this.fontChange('Cookie')
+}
+private fontChange(action:string){
+  this.elem.nativeElement.style.fontFamily = action
+  this.elem.nativeElement.style.transitionDuration = '0.5s'
+}
 }
