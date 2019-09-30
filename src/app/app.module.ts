dab63caa83489e8e55b04c050faec5a2611aa921
bchizi <leonbichanga@gmail.com>
2019-09-30 08:42:02 +0300

    Add component user html file and its layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GithubUserComponent } from './github-user/github-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    GithubUserComponent
   ],
   imports: [
     BrowserModule,
